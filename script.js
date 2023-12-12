5a4f6e1ed09bca1caac6a1e9c8c8e89d73b7e96e
Websitecodeatoms <websitecodeatoms@gmail.com>
2023-12-12 23:35:54 +0530

    eyeball code
    eyeball code

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ setInterval(()=>{
     sc.style.transform = `rotateZ(${ss}deg)`;
 })
 
+
+
+// clock script here
+
+document.querySelector('body').addEventListener('mousemove', eyeball)
+
+function eyeball(){
+    const eye = document.querySelectorAll('.eye');
+    eye.forEach((eye)=>{
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+
+        let radian = Math.atan2(event.pageX - x, event.pageY - y );
+        let rotation = (radian * (180 / Math.PI) * -1) + 270;
+
+        eye.style.transform = "rotate("+rotation+"deg)"
+    })
+}
